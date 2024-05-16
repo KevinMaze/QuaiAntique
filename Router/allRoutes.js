3f75a3151bc4fd587f11128f5a8f869a95914818
Kévin MAZE <kekeproject@hotmail.fr>
2024-05-16 20:20:15 +0200

    add connect and disconnect

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "./pages/home.html"),
     new Route("/galerie", "La Galerie", "./pages/galerie.html"),
-    new Route("/signin", "Connexion", "./pages/auth/signin.html"),
+    new Route("/signin", "Connexion", "./pages/auth/signin.html", "/js/auth/signin.js"),
     new Route("/signup", "Connexion", "./pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "./pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "./pages/auth/editPassword.html"),
